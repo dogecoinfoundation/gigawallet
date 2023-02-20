f6577e1ae6a5d885c1545e3dc7150e7a808927e0
Timothy Stebbing <tim@dogecoinfoundation.org>
2023-02-20 18:51:19 +1100

    reorg the old webapi.go into it's own package (#23)
    * reorg the old webapi.go into it's own package

--- pkg/errors.go
+++ pkg/errors.go
@@ -2,7 +2,6 @@ package giga
 
 import (
 	"fmt"
-	"net/http"
 )
 
 type ErrorCode string
@@ -15,14 +14,6 @@ const (
 	UnknownError  ErrorCode = "unknown-error"
 )
 
-var httpCodeForError = map[string]int{
-	string(BadRequest):    400,
-	string(NotAvailable):  503,
-	string(NotFound):      404,
-	string(AlreadyExists): 500,
-	string(UnknownError):  500,
-}
-
 type ErrorInfo struct {
 	Code    ErrorCode // machine-readble ErrorCode enumeration
 	Message string    // human-readable debug message (in production, logged on the server only)
@@ -34,12 +25,4 @@ func (e *ErrorInfo) Error() string {
 
 func NewErr(code ErrorCode, format string, args ...any) error {
 	return &ErrorInfo{Code: code, Message: fmt.Sprintf(format, args...)}
-}
-
-func HttpStatusForError(code ErrorCode) int {
-	status, found := httpCodeForError[string(code)]
-	if !found {
-		status = http.StatusInternalServerError
-	}
-	return status
 }
