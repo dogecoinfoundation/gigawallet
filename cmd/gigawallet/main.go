9c7451add181090705bf53c4a1d1925ca92d84c1
Raffe <andrew@dogecoin.com>
2023-05-30 16:46:17 +1000

    Stubs for InvoiceUpdater and BalanceTracker receivers

--- cmd/gigawallet/main.go
+++ cmd/gigawallet/main.go
@@ -37,8 +37,8 @@ func main() {
 	bus := giga.NewMessageBus()
 	c.Service("MessageBus", bus)
 
-	// Configure loggers
-	receivers.SetupLoggers(c, bus, conf)
+	// Set up all configured receivers
+	receivers.SetUpReceivers(c, bus, conf)
 
 	// Set up the L1 interface to Core
 	l1_core, err := core.NewDogecoinCoreRPC(conf)
@@ -63,7 +63,7 @@ func main() {
 		panic(err)
 	}
 
-	// Start the PaymentBroker service
+	// Start the PaymentBroker service (deprecated)
 	pb := broker.NewPaymentBroker(conf, store)
 	c.Service("Payment Broker", pb)
 
---
+++ pkg/receivers/main.go
@@ -0,0 +1,22 @@
+package receivers
+
+import (
+	giga "github.com/dogecoinfoundation/gigawallet/pkg"
+	"github.com/dogecoinfoundation/gigawallet/pkg/conductor"
+)
+
+// Sets up standard receivers.
+func SetUpReceivers(cond *conductor.Conductor, bus giga.MessageBus, conf giga.Config) {
+	// Set up configured loggers
+	SetupLoggers(cond, bus, conf)
+
+	// InvoiceUpdater marks invoices paid when transactions have fully paid them.
+	iup := NewInvoiceUpdater()
+	cond.Service("InvoiceUpdater", iup)
+	bus.Register(iup, giga.ACC_CHAIN_ACTIVITY)
+
+	// BalanceTracker updates wallet balances after transactions are confirmed.
+	btr := NewBalanceTracker()
+	cond.Service("BalanceTracker", btr)
+	bus.Register(iup, giga.ACC_CHAIN_ACTIVITY)
+}
