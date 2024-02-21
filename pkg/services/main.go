580358fb25027216e623452e2ad1efd3b06b47aa
Raffe <andrew@dogecoin.com>
2024-02-22 10:26:22 +1100

    Fix: shut down cleanly, remove unused service stubs

--- pkg/services/main.go
+++ pkg/services/main.go
@@ -9,12 +9,4 @@ func StartServices(cond *conductor.Conductor, bus giga.MessageBus, conf giga.Con
 	// BalanceKeeper updates stored balances and sends ACC_BALANCE_CHANGE events.
 	keeper := NewBalanceKeeper(store, bus)
 	cond.Service("NewBalanceKeeper", keeper)
-
-	// InvoiceStamper sends "Invoice Paid" and "Invoice Partial Payment" events.
-	stamper := NewInvoiceStamper()
-	cond.Service("InvoiceStamper", stamper)
-
-	// PayMaster sends "Payment Accepted" and "Payment Confirmed" events.
-	master := NewPayMaster()
-	cond.Service("PayMaster", master)
 }
